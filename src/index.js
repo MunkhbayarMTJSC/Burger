30626da7c4f7c28502ce8751b228e5b7faf6846a
MunkhbayarMTJSC <munkhbayar.pch@gmail.com>
2025-12-24 18:35:03 +0800

    Base route name nemsen

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Burger">
         <App />
       </BrowserRouter>
     </Provider>
